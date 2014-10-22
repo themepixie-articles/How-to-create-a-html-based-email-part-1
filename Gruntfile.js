289f3abd3aa4e6680cff65942997db0900c9265f
stephen.xzien <stephen@xzien.com>
2014-10-22 07:18:43 +0100

    Mailgun has been added

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,13 +11,26 @@ module.exports = function(grunt) {
 			files: {
          			'email-to-send.html': 'index.html'
          		}
-      		}
+      		},
+		mailgun: {
+			mailer: {
+				options: {
+					key: "Your API Key",
+					sender: "noreply@mywebsite.com",
+					recipient: "Your Email",
+					subject: "Test email"
+				},
+				src: ["email-to-send.html"]
+			}
+		}
 	});
 
 	grunt.loadNpmTasks('grunt-connect');
 	grunt.loadNpmTasks('grunt-inline-css');
+	grunt.loadNpmTasks('grunt-mailgun');
 
 	grunt.registerTask('serve', 'connect:local');
 	grunt.registerTask('convert', 'inlinecss');
+	grunt.registerTask('send', 'mailgun');
 	grunt.registerTask('default', 'serve');
 };
